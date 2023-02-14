3975cf110cdd3adc30aba076ca33cf1fd4e9cbc4
Enzoluizb <enzoluiz.brt@gmail.com>
2023-02-14 15:04:44 -0300

    update homescreen

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -4,7 +4,7 @@ import { styles } from "../lib/styles";
 export const HomeScreen = ({navigation}) =>{
     return(
         <View style={styles.container}>
-            <Text>Olá sou eu denovo</Text>
+            <Text>Home page</Text>
             <Button title='Ir para Sobre'
              onPress={() => navigation.navigate("Sobre")}></Button> 
            </View>
--- src/Screens/SobreScreen.jsx
+++ src/Screens/SobreScreen.jsx
@@ -3,7 +3,7 @@ import { Button, Text, View } from "react-native"
 export const SobreScreen =({navigation}) =>{
     return(
         <View>
-            <Text>AAAAAAAAA</Text>
+            <Text>Sobre</Text>
             <Button title = "volte" onPress={() =>navigation.navigate("Home")}
             ></Button>
         </View>
